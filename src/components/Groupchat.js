5dfe2accf1dcff915a58efdcaa07d670d1297dcc
Nofar <nofarna@post.bgu.ac.il>
2020-03-31 19:20:33 +0300

    init

---
+++ src/components/Groupchat.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Groupchat extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return <div className="chatWindow">
+            <h1>Login2</h1>
+        </div>;
+    }
+}
+export default Groupchat;
